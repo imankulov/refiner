2fa49e10179c2756863a84cdc207e7259c2a0fd8
Roman Imankulov <roman.imankulov@gmail.com>
2023-06-10 16:13:14 +0100

    Add a favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,24 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Prose Polish',
-  description: 'A tool to help you polish your prose.',
-}
+  title: "Prose Polish",
+  description: "A tool to help you polish your prose.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
