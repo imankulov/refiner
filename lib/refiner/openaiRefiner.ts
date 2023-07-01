9bfd73bff8a2a75595c214306526c1b42e9746ba
Roman Imankulov <roman.imankulov@gmail.com>
2023-07-01 10:10:56 +0100

    Remove console.log()

--- lib/refiner/openaiRefiner.ts
+++ lib/refiner/openaiRefiner.ts
@@ -22,19 +22,12 @@ ${formatInstructions(instructions)}
 - If you don't see any errors in the provided text and there is nothing to fix, return the provided text verbatim.
 `;
 
-  const formattedText = `
-${text}
-`;
-
-  console.log("prompt", prompt);
-  console.log("formattedText", formattedText);
-
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo-0613",
     temperature: 0,
     messages: [
       { role: "system", content: prompt },
-      { role: "user", content: formattedText },
+      { role: "user", content: text },
     ],
   });
   const completionText = completion.data.choices[0].message?.content;
