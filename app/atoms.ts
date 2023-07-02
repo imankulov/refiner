46dc723fffa74747df40278df34cd11f5fb32736
Roman Imankulov <roman.imankulov@gmail.com>
2023-07-02 14:38:31 +0100

    Add demo text

--- app/atoms.ts
+++ app/atoms.ts
@@ -1,9 +1,20 @@
 import { InstructionName } from "@/lib/refiner/instructions";
 import { atomWithStorage } from "jotai/utils";
 
-export const textAtom = atomWithStorage("text", "");
+const DEMO_TEXT = `Welcome to Refiner. Here's a quik demo:
+
+- Replace this text with your own.
+- Optionally, choose tone.
+- Click the 'Refine' button.
+- Watch as Refiner improves your text.
+
+Give it a try now.`;
+
+const DEMO_INSTRUCTION_NAMES: InstructionName[] = ["short", "simple"];
+
+export const textAtom = atomWithStorage("text", DEMO_TEXT);
 export const instructionNamesAtom = atomWithStorage<InstructionName[]>(
   "instructionNames",
-  []
+  DEMO_INSTRUCTION_NAMES
 );
 export const showDiffAtom = atomWithStorage("showDiff", true);
