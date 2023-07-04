2f9b496d596b57b2859620907f523bb9c3033cc4
Roman Imankulov <roman.imankulov@gmail.com>
2023-07-04 14:11:59 +0100

    Add language detection and custom language instructions
    An extra attempt to prevent GPT from translating the text.

--- lib/refiner/openaiRefiner.ts
+++ lib/refiner/openaiRefiner.ts
@@ -1,11 +1,15 @@
 import { Configuration, OpenAIApi } from "openai";
 import { Instruction } from "./instructions";
 
+import LanguageDetect from "languagedetect";
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY || "",
 });
 
 const openai = new OpenAIApi(configuration);
+
+const languageDetector = new LanguageDetect();
 
 export async function openAIRefineText(
   text: string,
@@ -17,7 +21,7 @@ The output text must conform to the following instructions:
 
 ${formatInstructions(instructions)}
 - Return only corrected text. Do not write validation status.
-- Keep the output language the same as the input language. Do not translate the text.
+- ${getLanguageInstruction(text)} Do not translate the text.
 - Do not add any information that is not present in the input text.
 - If you don't see any errors in the provided text and there is nothing to fix, return the provided text verbatim.
 `;
@@ -32,6 +36,26 @@ ${formatInstructions(instructions)}
   });
   const completionText = completion.data.choices[0].message?.content;
   return completionText || "";
+}
+
+function getLanguageInstruction(text: string): string {
+  const results = languageDetector.detect(text, 1);
+  const fallbackInstruction =
+    "Keep the output language the same as the input language.";
+  if (results.length === 0) {
+    return fallbackInstruction;
+  }
+  const [languageName, prob] = results[0];
+  if (prob < 0.1) {
+    return fallbackInstruction;
+  }
+  return `Keep ${titleCase(
+    languageName
+  )} as the output language (the same as the input language).`;
+}
+
+function titleCase(value: string): string {
+  return value.charAt(0).toUpperCase() + value.slice(1);
 }
 
 function formatInstructions(instructions: Instruction[]): string {
