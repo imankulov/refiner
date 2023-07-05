2c9499c35553d0338363c942f3860f041e35fd8e
Roman Imankulov <roman.imankulov@gmail.com>
2023-07-05 16:03:00 +0100

    Update demo text

--- app/atoms.ts
+++ app/atoms.ts
@@ -1,16 +1,29 @@
 import { InstructionName } from "@/lib/refiner/instructions";
 import { atomWithStorage } from "jotai/utils";
 
-const DEMO_TEXT = `Welcome to Refiner. Here's a quik demo:
+const DEMO_TEXT = `Welcome to Refiner. 
+
+Refiner improves your writings. It corrects grammar issues, adjusts tone, and offers formatting options. 
+I found it useful for non-native speakers and professionals who communicate with text.
+
+Here's a quick demo:
 
 - Replace this text with your own.
-- Optionally, choose tone.
+- Optionally, choose tone and refinement instructions.
 - Click the 'Refine' button.
-- Watch as Refiner improves your text.
+- Watch as Refiner XXX.
+- Copy XXX
+
+It is an open-source project. The code is available on https://github.com/imankulov/refiner.
 
 Give it a try now.`;
 
-const DEMO_INSTRUCTION_NAMES: InstructionName[] = ["short", "simple"];
+const DEMO_INSTRUCTION_NAMES: InstructionName[] = [
+  "short",
+  "simple",
+  "markdown",
+  "fillBlanks",
+];
 
 export const textAtom = atomWithStorage("text", DEMO_TEXT);
 export const instructionNamesAtom = atomWithStorage<InstructionName[]>(
