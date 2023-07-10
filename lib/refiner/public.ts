8d9e0d530b6ed7d3eb0d8dc9a75f36508607ba04
Roman Imankulov <roman.imankulov@gmail.com>
2023-07-10 19:07:19 +0100

    Make openAI refined default

--- lib/refiner/public.ts
+++ lib/refiner/public.ts
@@ -2,11 +2,14 @@ import { mockRefineText } from "./mockRefiner";
 import { openAIRefineText } from "./openaiRefiner";
 import { Instruction } from "./instructions";
 
+const defaultRefiner = "openai";
+
 export async function refineText(
   text: string,
   instructions: Instruction[]
 ): Promise<string> {
-  if (process.env.REFINER === "openai") {
+  const refiner = process.env.REFINER ?? defaultRefiner;
+  if (refiner === "openai") {
     return openAIRefineText(text, instructions);
   } else {
     return mockRefineText(text, instructions);
