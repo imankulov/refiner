ab40b0a8e786553dd06e67302de07554dc2dea6d
Roman Imankulov <roman.imankulov@gmail.com>
2024-02-23 22:59:35 +0000

    Make SHOW_SUPPORT_UKRAINE_BANNER a server-side var

---
+++ app/Home.tsx
@@ -0,0 +1,81 @@
+"use client";
+import Stack from "@mui/material/Stack";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import { RefinerSnackbar } from "@/components/RefinerSnackbar";
+import { Editor } from "@/components/Editor";
+import { RefinedArea } from "@/components/RefinedArea";
+import { useHotkeys } from "react-hotkeys-hook";
+import { useRefine } from "./hooks/useRefine";
+import { useCopyRefinedContent } from "./hooks/useCopyRefinedContent";
+import { useToggleDiff } from "./hooks/useToggleDiff";
+import type { Options } from "react-hotkeys-hook/dist/types";
+import { Header } from "@/components/Header";
+import { InstructionSelector } from "@/components/InstructionSelector";
+import { InstructionsToolbar } from "@/components/InstructionsToolbar";
+import { SupportUkraineBanner } from "@/components/SupportUkraineBanner";
+
+export function Home({
+  showSupportUkraineBanner,
+}: {
+  showSupportUkraineBanner: boolean;
+}) {
+  const refine = useRefine();
+  const copyRefinedContent = useCopyRefinedContent();
+  const toggleDiff = useToggleDiff();
+
+  const hotkeyOptions: Options = {
+    enableOnFormTags: true,
+    preventDefault: true,
+  };
+
+  useHotkeys("mod+enter", refine, hotkeyOptions);
+  useHotkeys("mod+shift+c", copyRefinedContent, hotkeyOptions);
+  useHotkeys("mod+shift+d", toggleDiff, hotkeyOptions);
+
+  return (
+    <Container
+      maxWidth="lg"
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        p: {
+          xs: 1,
+          md: 2,
+        },
+      }}
+    >
+      {showSupportUkraineBanner && <SupportUkraineBanner />}
+      <Header>
+        <InstructionSelector />
+        <InstructionsToolbar />
+      </Header>
+      <Stack
+        spacing={{
+          xs: 1,
+          md: 2,
+        }}
+        direction={{
+          xs: "column",
+          md: "row",
+        }}
+        sx={{
+          flexGrow: 1,
+        }}
+      >
+        <Paper
+          sx={{ p: 2, flexGrow: 1, width: { md: "50%" }, display: "flex" }}
+        >
+          <Editor />
+        </Paper>
+        <Paper
+          sx={{ p: 2, flexGrow: 1, width: { md: "50%" }, display: "flex" }}
+        >
+          <RefinedArea />
+        </Paper>
+      </Stack>
+      <RefinerSnackbar />
+    </Container>
+  );
+}
